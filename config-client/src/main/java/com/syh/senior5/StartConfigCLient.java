1bfe57b7972a3c28887155a9744aaae8cc670be2
songyinghui <songyinghui>
2020-06-23 16:04:24 +0800

    第一次提交

---
+++ config-client/src/main/java/com/syh/senior5/StartConfigCLient.java
@@ -0,0 +1,11 @@
+package com.syh.senior5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartConfigCLient {
+    public static void main(String[] args) {
+        SpringApplication.run(StartConfigCLient.class,args);
+    }
+}
